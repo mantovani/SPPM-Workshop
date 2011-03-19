7433643d9dfcd56866ce39c36d187464390a420c
Eden Cardim <edencardim@gmail.com>
2011-03-19 20:10:38 -0300

    funcionalidade de inscricoes

--- lib/SPPM/WorkShop/Controller/Inscricao.pm
+++ lib/SPPM/WorkShop/Controller/Inscricao.pm
@@ -7,12 +7,34 @@ BEGIN { extends 'Catalyst::Controller'; }
 
 sub base : Chained('/base') : PathPart('inscricao') : CaptureArgs(0) {
     my ( $self, $c ) = @_;
-    $c->stash(
-        planos => $c->model('DB::Planos')
-    );
+    $c->stash->{inscricoes} = $c->model('DB::Inscricao');
 }
 
-sub inscricao : Chained('base') : PathPart('') : Args(0) {}
+sub object :Chained('base') PathPart('') CaptureArgs(0) {
+    my($self, $c) = @_;
+    my $rs = $c->stash->{inscricoes};
+    my $obj = $c->stash->{inscricao} = $rs->find_or_new($c->req->body_parameters);
+}
+
+sub inscricao : Chained('object') PathPart('') Args(0) {
+    my($self, $c) = @_;
+    my $params = $c->req->body_parameters;
+
+    $c->stash->{mensagem}{nome} = q{*};
+    $c->stash->{erro}{nome} = q{Favor fornecer o nome completo}
+        unless $params->{nome};
+
+    $c->stash->{mensagem}{email} = q{*};
+    $c->stash->{erro}{email} = q{Favor fornecer um email}
+        unless $params->{email};
+
+    $c->stash->{mensagem}{celular} = q{*};
+    $c->stash->{erro}{celular} = q{Favor fornecer o celular}
+        unless $params->{celular};
+
+    return if %{$c->stash->{erro} || {}};
+    $c->stash->{inscricao}->insert if $c->req->method eq 'POST';
+}
 
 sub cadastro : Chained('base') : Args(0) {
     my ( $self, $c ) = @_;
